edd34752593d92262b873822b0a516bf6597d295
yangsong13 <yangsong13@meituan.com>
2022-12-25 14:57:57 +0800

    add dist

---
+++ docs/precache-manifest.e3a0cde409b41dd9f1ad1e03c5157d2e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae0a0c055978acbe132",
+    "url": "/css/app.de6b0c71.css"
+  },
+  {
+    "revision": "7f1be693d81b074ed87e",
+    "url": "/css/chunk-vendors.532634c1.css"
+  },
+  {
+    "revision": "8ebb3fab7ceabbdef8fa",
+    "url": "/css/practice.691b3b29.css"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/fonts/digital-7-mono.58045dab.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "4e21247f44a796061adfb5697981a5de",
+    "url": "/img/layouts/QWERTY.png"
+  },
+  {
+    "revision": "9683f7fb1fad48dcd284f03709b7a6df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ff2c7dc1429fc9cd2f8",
+    "url": "/js/about.f43081cf.js"
+  },
+  {
+    "revision": "5cab8e12a4d3f35a2a50",
+    "url": "/js/about~changelog.45d01fc0.js"
+  },
+  {
+    "revision": "dae0a0c055978acbe132",
+    "url": "/js/app.3f48e377.js"
+  },
+  {
+    "revision": "c63c7b18da3912a91f05",
+    "url": "/js/changelog.3a2a2195.js"
+  },
+  {
+    "revision": "7f1be693d81b074ed87e",
+    "url": "/js/chunk-vendors.461983e5.js"
+  },
+  {
+    "revision": "8ebb3fab7ceabbdef8fa",
+    "url": "/js/practice.c24e0cf1.js"
+  },
+  {
+    "revision": "8c7a5fe0ca74b014af89",
+    "url": "/js/setting.c58c9d0a.js"
+  },
+  {
+    "revision": "5549507588797b4f32ad",
+    "url": "/js/summary.2e3483f5.js"
+  },
+  {
+    "revision": "7f3be8dcb2172491a8b0a606cd563597",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
