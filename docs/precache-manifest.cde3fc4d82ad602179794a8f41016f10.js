0ea692f663105363e9e23644f1216f9c3bd798d1
yangsong13 <yangsong13@meituan.com>
2022-12-27 16:17:53 +0800

    rebuild

---
+++ docs/precache-manifest.cde3fc4d82ad602179794a8f41016f10.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5e22e4b5249ab7282c5",
+    "url": "/easy-typer/css/app.a25ad26c.css"
+  },
+  {
+    "revision": "c8100746fcaee5c5715c",
+    "url": "/easy-typer/css/chunk-vendors.532634c1.css"
+  },
+  {
+    "revision": "d68560c03bdf737742d9",
+    "url": "/easy-typer/css/practice.691b3b29.css"
+  },
+  {
+    "revision": "58045dabdc3a361cb9bb9faf2f1dd1f3",
+    "url": "/easy-typer/fonts/digital-7-mono.58045dab.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/easy-typer/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/easy-typer/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "4e21247f44a796061adfb5697981a5de",
+    "url": "/easy-typer/img/QWERTY.4e21247f.png"
+  },
+  {
+    "revision": "2a05742a1968ecea77d81b7b44b80766",
+    "url": "/easy-typer/img/logo-landscape-white@2x.png"
+  },
+  {
+    "revision": "0244ce5dadb1dc256d7dd5086ccd1d12",
+    "url": "/easy-typer/img/logo-lanscape@2x.png"
+  },
+  {
+    "revision": "3ee07ddac08e38420ccf43a9a5ba7d6e",
+    "url": "/easy-typer/img/setting.png"
+  },
+  {
+    "revision": "126d4aa318e52fb11583e83acb94baaa",
+    "url": "/easy-typer/index.html"
+  },
+  {
+    "revision": "fd7b70361c4602703045",
+    "url": "/easy-typer/js/about.ee5d7acf.js"
+  },
+  {
+    "revision": "5cab8e12a4d3f35a2a50",
+    "url": "/easy-typer/js/about~changelog.45d01fc0.js"
+  },
+  {
+    "revision": "a5e22e4b5249ab7282c5",
+    "url": "/easy-typer/js/app.80ab6fd8.js"
+  },
+  {
+    "revision": "8305c856dfe4f8261104",
+    "url": "/easy-typer/js/changelog.405501f6.js"
+  },
+  {
+    "revision": "c8100746fcaee5c5715c",
+    "url": "/easy-typer/js/chunk-vendors.8e6c4d5a.js"
+  },
+  {
+    "revision": "d68560c03bdf737742d9",
+    "url": "/easy-typer/js/practice.cbc24fff.js"
+  },
+  {
+    "revision": "806414bf9fe84223e440",
+    "url": "/easy-typer/js/setting.a5dd6ca1.js"
+  },
+  {
+    "revision": "f2da300f1a0679b21fce",
+    "url": "/easy-typer/js/summary.c7b4fd88.js"
+  },
+  {
+    "revision": "7f3be8dcb2172491a8b0a606cd563597",
+    "url": "/easy-typer/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easy-typer/robots.txt"
+  }
+]);
