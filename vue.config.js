01e957e36285c5898cb1ffab43a489030ac77636
yangsong13 <yangsong13@meituan.com>
2023-01-24 20:38:55 +0800

    add sitemap

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
+const sitemapPlugin = require('sitemap-webpack-plugin').default
+
+const routes = [
+  '/',
+  '/practice',
+  '/kata',
+  '/setting',
+  '/summary',
+  '/changelog',
+  '/help',
+  '/about',
+  '/history',
+  '/download'
+]
+
 process.env.VUE_APP_VERSION = require('./package.json').version
 process.env.VUE_APP_WEB_VERSION = '(0.7)'
 
@@ -25,25 +40,24 @@ module.exports = {
       .use('raw-loader')
       .loader('raw-loader')
       .end()
+    config.plugin('sitemap').use(sitemapPlugin, [
+      {
+        base: 'https://typer.owenyang.top',
+        paths: routes,
+        options: {
+          filename: 'sitemap.xml',
+          lastMod: true,
+          changefreq: 'weekly'
+        }
+      }
+    ])
   },
-
   productionSourceMap: false,
 
   pluginOptions: {
     prerenderSpa: {
       registry: undefined,
-      renderRoutes: [
-        '/',
-        '/practice',
-        '/kata',
-        '/setting',
-        '/summary',
-        '/changelog',
-        '/help',
-        '/about',
-        '/history',
-        '/download'
-      ],
+      renderRoutes: routes,
       useRenderEvent: true,
       headless: true,
       onlyProduction: true
