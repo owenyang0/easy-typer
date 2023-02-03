ec88ff7da8050b0451d4c2ef2383920e610841a2
yangsong13 <yangsong13@meituan.com>
2023-02-03 12:47:31 +0800

    update auth scroll

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 process.env.VUE_APP_VERSION = require('./package.json').version
-process.env.VUE_APP_WEB_VERSION = '(11)'
+process.env.VUE_APP_WEB_VERSION = '(12)'
 
 const name = '木易跟打器'
 
