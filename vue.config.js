c19e91901c457152474808f6b984fe0ea2ceb8c0
yangsong13 <yangsong13@meituan.com>
2023-02-07 16:12:44 +0800

    update to web14

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const routes = [
 ]
 
 process.env.VUE_APP_VERSION = require('./package.json').version
-process.env.VUE_APP_WEB_VERSION = '(13)'
+process.env.VUE_APP_WEB_VERSION = '(14)'
 
 const name = '木易跟打器'
 
