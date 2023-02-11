eeb00f8f8caefb99445d3c6695c7ea184f71416c
yangsong13 <yangsong13@meituan.com>
2023-02-11 17:01:36 +0800

    添加文本

---
+++ scripts/config.js
@@ -0,0 +1,116 @@
+const config = [
+  {
+    name: '离骚',
+    value: 'LS'
+  },
+  {
+    name: '出师表',
+    value: 'CSB'
+  },
+  {
+    name: '洛神赋',
+    value: 'LSF'
+  },
+  {
+    name: '蜀道难',
+    value: 'SDN'
+  },
+  {
+    name: '长恨歌',
+    value: 'CHG'
+  },
+  {
+    name: '陈情表',
+    value: 'CQB'
+  },
+  {
+    name: '陋室铭',
+    value: 'LSM'
+  },
+  {
+    name: '将进酒·君不见',
+    value: 'JJJ'
+  },
+  {
+    name: '兰亭集序',
+    value: 'LTJX'
+  },
+  {
+    name: '前赤壁赋',
+    value: 'QCBF'
+  },
+  {
+    name: '后赤壁赋',
+    value: 'HCBF'
+  },
+  {
+    name: '小石潭记',
+    value: 'XSTJ'
+  },
+  {
+    name: '岳阳楼记',
+    value: 'YYLJ'
+  },
+  {
+    name: '捕蛇者说',
+    value: 'BSZH'
+  },
+  {
+    name: '曾广贤文',
+    value: 'ZGXW'
+  },
+  {
+    name: '桃花源记',
+    value: 'THYJ'
+  },
+  {
+    name: '滕王阁序',
+    value: 'TWGX'
+  },
+  {
+    name: '登泰山记',
+    value: 'DTSJ'
+  },
+  {
+    name: '醉翁亭记',
+    value: 'ZWTJ'
+  },
+  {
+    name: '阿房宫赋',
+    value: 'APGF'
+  },
+  {
+    name: '项脊轩志',
+    value: 'XJXZ'
+  },
+  {
+    name: '与朱元思书',
+    value: 'YZYSS'
+  },
+  {
+    name: '湖心亭看雪',
+    value: 'HXTKX'
+  },
+  {
+    name: '祭十二郎文',
+    value: 'JSELW'
+  },
+  {
+    name: '邹忌讽齐王纳谏',
+    value: 'ZJFQWNJ'
+  },
+  {
+    name: '木兰诗（木兰辞）',
+    value: 'MLS'
+  },
+  {
+    name: '春夜宴从弟桃花园序',
+    value: 'CYYCDTHYX'
+  },
+  {
+    name: '冰灯',
+    value: 'BD'
+  }
+]
+
+module.exports = config
