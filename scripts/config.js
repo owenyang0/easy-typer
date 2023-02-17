e83b32e0f989db6033f78417df1f54d7e9828398
yangsong13 <yangsong13@meituan.com>
2023-02-17 09:55:14 +0800

    更新单字

--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,4 @@
-const config = [
+const articleConfig = [
   {
     name: '离骚',
     value: 'LS'
@@ -113,4 +113,22 @@ const config = [
   }
 ]
 
-module.exports = config
+const singleConfig = [
+  {
+    name: '王500 3501-4000',
+    value: 'Wang500'
+  },
+  {
+    name: '皇500 4001-4500',
+    value: 'Grand500'
+  },
+  {
+    name: '帝500 4501-5000',
+    value: 'Empire500'
+  }
+]
+
+module.exports = {
+  articleConfig,
+  singleConfig
+}
