5c15963adaad04768330b678acd1ad950c3bc8a1
yangsong13 <yangsong13@meituan.com>
2023-02-18 23:36:45 +0800

    update config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const sitemapPlugin = require('sitemap-webpack-plugin').default
 
 const routes = [
-  '/app',
+  '/',
   '/practice',
   '/kata',
   '/reading',
@@ -32,10 +32,32 @@ module.exports = {
       msTileImage: 'img/icons/msapplication-icon-144x144.png'
     },
     appleMobileWebAppCapable: 'yes',
-    assetsVersion: version,
+    assetsVersion: '26.1',
     display: 'standalone',
     themeColor: '#1c1f24',
-    msTileColor: '#1c1f24'
+    msTileColor: '#1c1f24',
+    // // configure the workbox plugin
+    // workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      // swSrc: 'dev/sw.js'
+      // ...other Workbox options...
+      runtimeCaching: [
+        {
+          urlPattern: '/',
+          handler: 'StaleWhileRevalidate',
+          options: {
+            cacheName: 'easy-typer-custom-cache',
+            expiration: {
+              maxAgeSeconds: 86400 * 30
+            },
+            cacheableResponse: {
+              statuses: [0, 200]
+            }
+          }
+        }
+      ]
+    }
   },
 
   publicPath: process.env.NODE_ENV === 'production'
@@ -68,7 +90,6 @@ module.exports = {
     ])
   },
   productionSourceMap: false,
-
   pluginOptions: {
     prerenderSpa: {
       registry: undefined,
