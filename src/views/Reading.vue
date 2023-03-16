c09af3fb2d65a28038b14e188c61186fb845ba04
yangsong13 <yangsong13@meituan.com>
2023-03-16 13:59:11 +0800

    update to web41

--- src/views/Reading.vue
+++ src/views/Reading.vue
@@ -262,7 +262,7 @@ export default class Reading extends Vue {
             id,
             title,
             totalWords,
-            paragraphSize: 200,
+            paragraphSize: 500,
             currentWords: 0,
             paragraphs: Math.ceil(result.length / 10)
           }
