2b803cb7158a66fb8befd145e13e79b2b920f664
yangsong13 <yangsong13@meituan.com>
2023-03-24 14:53:00 +0800

    优化请求过程

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,7 @@ const routes = [
   '/download'
 ]
 
-const version = '(51)'
+const version = '(52)'
 process.env.VUE_APP_VERSION = require('./package.json').version
 process.env.VUE_APP_WEB_VERSION = version
 
