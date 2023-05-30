7ee28da8235f5cefc6d1550d863c532fe04bb4a7
yangsong13 <yangsong13@meituan.com>
2023-05-30 16:45:38 +0800

    add new article

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ const routes = [
   '/buy-me-a-coffee'
 ]
 
-const version = '(60)'
+const version = '(61)'
 process.env.VUE_APP_VERSION = require('./package.json').version
 process.env.VUE_APP_WEB_VERSION = version
 
