6bf19bad6e67393987afe86eb99164eb7bb818b8
yangsong13 <yangsong13@meituan.com>
2024-04-16 20:02:04 +0800

    feat: update citi

---
+++ scripts/sort_citi.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 检查是否提供了足够的参数
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <input_file> <output_file>"
+    exit 1
+fi
+
+# 读取输入参数
+input_file=$1
+output_file=$2
+
+# 检查输入文件是否存在
+if [ ! -f "$input_file" ]; then
+    echo "Error: The file $input_file does not exist."
+    exit 1
+fi
+
+# 使用sort命令对文件进行排序，-k2指定按照第二列排序，-o指定输出文件
+sort -k2 "$input_file" -o "$output_file" -s
+
+# ./sort_citi.sh ./codings/single.txt ../public/static/coding24.04.10.txt
+# ./sort_citi.sh ./codings/words.txt ../public/static/codingCi24.04.10.txt
--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -63,8 +63,8 @@
               </div>
               <div class="el-upload__tip" slot="tip">3. 点击更新默认编码提示：
                 <!-- <el-button-group> -->
-                <el-button type="primary" plain size="mini" icon="el-icon-download" :loading="isCodingLoading" @click="handleCodingDownload('tiger')">『虎码单字2024元旦版』</el-button>
-                <el-button type="primary" plain size="mini" icon="el-icon-download" :loading="isCodingLoading" @click="handleCodingDownload('tigerCi')">『虎码字词2024元旦版』</el-button>
+                <el-button type="primary" plain size="mini" icon="el-icon-download" :loading="isCodingLoading" @click="handleCodingDownload('tiger')">『虎码单字2024.04.10』</el-button>
+                <el-button type="primary" plain size="mini" icon="el-icon-download" :loading="isCodingLoading" @click="handleCodingDownload('tigerCi')">『虎码字词2024.04.10』</el-button>
                 <el-button type="primary" plain size="mini" icon="el-icon-download" :loading="isCodingLoading" @click="handleCodingDownload('xh')">『小鹤词提』</el-button>
                 <!-- </el-button-group> -->
               </div>
@@ -446,11 +446,11 @@ export default class Setting extends Vue {
       },
       tiger: {
         title: '『虎码单字』',
-        url: '/static/codings24.01.01.txt'
+        url: '/static/codings24.04.10.txt'
       },
       tigerCi: {
         title: '『虎码字词』',
-        url: '/static/codingsci24.01.01.txt'
+        url: '/static/codingsCi24.04.10.txt'
       }
     }
     const codingType = codingsTypes[type]
