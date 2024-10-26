daf3961af1ba60f2d6157589e0b41c9e922e892e
yangsong13 <yangsong13@meituan.com>
2024-10-26 14:54:03 +0800

    update sitemap

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,20 @@ const routes = [
   '/about',
   '/history',
   '/download',
-  '/buy-me-a-coffee'
+  '/buy-me-a-coffee',
+  '/portal/blog/',
+  '/portal/blog/easy-typer-macos-vs-web-app/',
+  '/portal/blog/easy-typer-working-in-macos-platform/',
+  '/portal/blog/tags/',
+  '/portal/blog/tags/mac%E8%B7%9F%E6%89%93%E5%99%A8/',
+  '/portal/blog/tags/qq%E8%B7%9F%E6%89%93%E5%99%A8/',
+  '/portal/blog/tags/%E6%9C%A8%E6%98%93%E8%B7%9F%E6%89%93%E5%99%A8/',
+  '/portal/blog/tags/%E8%B7%9F%E6%89%93%E5%99%A8/',
+  '/portal/docs/changelog/',
+  '/portal/docs/get-started/about/',
+  '/portal/docs/get-started/changelog/',
+  '/portal/docs/guides/download/',
+  '/portal/docs/intro/'
 ]
 
 const version = '(73)'
